4709fc969711dcc4a19ed5ca8eaac781ec3221fc
Ahmed Nasr <ahmednasrhasan811@gmail.com>
2024-11-28 00:17:11 +0200

    Admin parts and connection updates

--- Package_Tracking_Backend/models/order.go
+++ Package_Tracking_Backend/models/order.go
@@ -4,9 +4,9 @@ type Order struct {
 	ID              int64  `db:"id" json:"id"`
 	UserID          int64  `db:"user_id" json:"user_id"`
 	CourierID       int64  `db:"courier_id" json:"courier_id"`
-	PickupLocation  string `db:"pickup_location" json:"pickup_location"`
-	DropoffLocation string `db:"dropoff_location" json:"dropoff_location"`
-	PackageDetails  string `db:"package_details" json:"package_details"`
-	DeliveryTime    string `db:"delivery_time" json:"delivery_time"`
+	PickupLocation  string `db:"pickup_location" json:"pickupLocation"`
+    DropoffLocation string `db:"dropoff_location" json:"dropoffLocation"`
+    PackageDetails  string `db:"package_details" json:"packageDetails"`
+    DeliveryTime    string `db:"delivery_time" json:"deliveryTime"`
 	Status          string `db:"status" json:"status"`
 }
