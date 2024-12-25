2e930d10acb39b543542ef47372eb2dc7cf41778
AhmedNasr811 <ahmednasrhasan811@gmail.com>
2024-12-25 02:28:21 +0200

    update

--- Package_Tracking_Backend/db/db.go
+++ Package_Tracking_Backend/db/db.go
@@ -3,7 +3,7 @@ package db
 import (
 	"fmt"
 	"log"
-
+	"os"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
@@ -13,12 +13,33 @@ var DB *sqlx.DB
 // The name of the db is changed to include the whole data not only the users
 // InitDB initializes the database connection
 func InitDB() {
-	connStr := "user=postgres password=123 dbname=PackageTracking_db sslmode=disable"
-	var err error
-	DB, err = sqlx.Connect("postgres", connStr)
-	if err != nil {
-		log.Fatalf("Error connecting to the database, try again: %v", err)
-	}
+	//connStr := "user=postgres password=123 dbname=PackageTracking_db sslmode=disable"
+	//var err error
+	//DB, err = sqlx.Connect("postgres", connStr)
+	//if err != nil {
+	//	log.Fatalf("Error connecting to the database, try again: %v", err)
+	//}
 
-	fmt.Println("Connected to the database, great!")
+	//fmt.Println("Connected to the database, great!")
+
+	dbHost := os.Getenv("DB_HOST")
+    dbPort := os.Getenv("DB_PORT")
+    dbUser := os.Getenv("DB_USER")
+    dbPassword := os.Getenv("DB_PASSWORD")
+    dbName := os.Getenv("DB_NAME")
+
+    // Build the connection string
+    connStr := fmt.Sprintf(
+        "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+        dbHost, dbPort, dbUser, dbPassword, dbName,
+    )
+
+    var err error
+    DB, err = sqlx.Connect("postgres", connStr)
+    if err != nil {
+        log.Fatalf("Error connecting to the database: %v", err)
+    }
+
+    fmt.Println("Connected to the database, great!")
+
 }
--- Package_Tracking_Backend/go.mod
+++ Package_Tracking_Backend/go.mod
@@ -1,6 +1,6 @@
 module Package_Tracking_Backend
 
-go 1.23.2
+go 1.22
 
 require (
 	github.com/gin-gonic/gin v1.10.0
